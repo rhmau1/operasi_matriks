08324f5c9c975ae4259e6913a723bdf4696bd1c6
Adammm <adambahymaulana7@gmail.com>
2024-12-08 16:25:06 +0700

    ayo cepet dimas

--- matriks.java
+++ matriks.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class matriks {
     static double[][] matriksHasil;
-    static double[][] matriks1;
-    static double[][] matriks2;
+    static int[][] matriks1;
+    static int[][] matriks2;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -44,8 +44,8 @@ public class matriks {
                         System.out.println("operasi tidak dapat dilakukan");
                         return;
                     }
-                    matriks1 = new double[baris1][kolom1];
-                    matriks2 = new double[baris2][kolom2];
+                    matriks1 = new int[baris1][kolom1];
+                    matriks2 = new int[baris2][kolom2];
                     perkalian(matriks1, matriks2, sc);
                     System.out.println("--- Hasil Perkalian ---");
                     printHasil(matriksHasil);
@@ -66,27 +66,17 @@ public class matriks {
         }
     }
 
-    public static double[][] input(Scanner sc, double[][] matriks) {
+    public static int[][] input(Scanner sc, int[][] matriks) {
         for (int i = 0; i < matriks.length; i++) {
             for (int j = 0; j < matriks[0].length; j++) {
                 System.out.print("Masukkan angka baris ke-" + (i + 1) + " kolom ke-" + (j + 1) + " : ");
-                matriks[i][j] = sc.nextDouble();
+                matriks[i][j] = sc.nextInt();
             }
         }
         return matriks;
     }
 
-    public static double[][] convertToDouble(double[][] intArray) {
-        double[][] doubleArray = new double[intArray.length][intArray[0].length];
-        for (int i = 0; i < intArray.length; i++) {
-            for (int j = 0; j < intArray[0].length; j++) {
-                doubleArray[i][j] = intArray[i][j];
-            }
-        }
-        return doubleArray;
-    }
-
-    public static void perkalian(double[][] matriks1, double[][] matriks2, Scanner sc) {
+    public static void perkalian(int[][] matriks1, int[][] matriks2, Scanner sc) {
         System.out.println("------------------------------");
         System.out.println("masukkan matriks 1: ");
         input(sc, matriks1);
@@ -104,74 +94,18 @@ public class matriks {
         }
     }
 
-    public static double[][] invers3x3(double[][] matriks) {
-        // Validasi ukuran matriks
-        if (matriks.length != 3 || matriks[0].length != 3) {
-            System.out.println("Matriks harus berukuran 3x3.");
-            return null; // Keluar jika matriks tidak valid
-        }
-    
-        // Elemen matriks
-        double a = matriks[0][0], b = matriks[0][1], c = matriks[0][2];
-        double d = matriks[1][0], e = matriks[1][1], f = matriks[1][2];
-        double g = matriks[2][0], h = matriks[2][1], i = matriks[2][2];
-    
-        // Hitung determinan
-        double determinan = (a * e * i + b * f * g + c * d * h)
-                          - (c * e * g + a * f * h + b * d * i);
-    
-        if (determinan == 0) {
-            System.out.println("Matriks tidak memiliki invers (determinan nol).");
-            return null; // Keluar jika determinan nol
-        }
-    
-        // Hitung kofaktor
-        double[][] kofaktor = {
-            { e * i - f * h, -(b * i - c * h), b * f - c * e },
-            { -(d * i - f * g), a * i - c * g, -(a * f - c * d) },
-            { d * h - e * g, -(a * h - b * g), a * e - b * d }
-        };
-    
-        // Transpose kofaktor untuk menghasilkan adjoin
-        double[][] adjoin = new double[3][3];
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-                adjoin[j][k] = kofaktor[k][j];
-            }
-        }
-    
-        // Hitung invers matriks
-        double[][] invers = new double[3][3];
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-                invers[j][k] = adjoin[k][j] / determinan;
-            }
-        }
-    
-        return invers;
-    }
-    
-
-    public static void printHasil(double[][] matriksHasil) {
-        for (int i = 0; i < matriksHasil.length; i++) {
-            for (int j = 0; j < matriksHasil[0].length; j++) {
-                System.out.print(matriksHasil[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void invers(double[][] matriks1, Scanner sc) {
+    public static void invers(int[][] matriks1, Scanner sc) {
         System.out.println("1. invers 2x2");
         System.out.println("2. invers 3x3");
         System.out.print("masukkan pilihan: ");
         int pilih = sc.nextInt();
         switch (pilih) {
             case 1:
+                System.out.println();
                 System.out.println("--- invers 2x2 ---");
-                System.out.println("------------------------------");
+                System.out.println("------------------");
                 System.out.println("masukkan elemen matriks: ");
-                matriks1 = new double[2][2];
+                matriks1 = new int[2][2];
                 input(sc, matriks1);
                 double determinan = determinan2x2(matriks1);
                 if (determinan == 0) {
@@ -183,14 +117,13 @@ public class matriks {
                 printHasil(invers2);
                 break;
             case 2:
-                System.out.println("--- invers 3x3 ---");
                 System.out.println();
-                matriks1 = new double[3][3];
-                System.out.println("Masukkan elemen matriks 3x3:");
-                // Konversi ke double[][] sebelum invers
-                double[][] invers3 = input(sc, matriks1);
-                double[][] hasil3 = invers3x3(invers3);
-
+                System.out.println("--- invers 3x3 ---");
+                System.out.println("------------------");
+                System.out.println("masukkan elemen matriks: ");
+                matriks1 = new int[3][3];
+                input(sc, matriks1);
+                double[][] hasil3 = invers3x3(matriks1);
                 System.out.println("--- Hasil Invers ---");
                 printHasil(hasil3);
                 break;
@@ -200,7 +133,7 @@ public class matriks {
         }
     }
 
-    public static double[][] invers2x2(double[][] matriks1, double determinan) {
+    public static double[][] invers2x2(int[][] matriks1, double determinan) {
         double[][] invers = new double[2][2];
         invers[0][0] = matriks1[1][1] / determinan; // d
         invers[0][1] = -matriks1[0][1] / determinan; // -b
@@ -209,8 +142,59 @@ public class matriks {
         return invers;
     }
 
-    public static double determinan2x2(double[][] matriks1) {
+    public static double determinan2x2(int[][] matriks1) {
         double determinan = matriks1[0][0] * matriks1[1][1] - matriks1[1][0] * matriks1[0][1];
         return determinan;
+    }
+
+    public static double[][] invers3x3(int[][] matriks) {
+
+        // Elemen matriks
+        double a = matriks[0][0], b = matriks[0][1], c = matriks[0][2];
+        double d = matriks[1][0], e = matriks[1][1], f = matriks[1][2];
+        double g = matriks[2][0], h = matriks[2][1], i = matriks[2][2];
+
+        // Hitung determinan
+        double determinan = (a * e * i + b * f * g + c * d * h)
+                - (c * e * g + a * f * h + b * d * i);
+
+        if (determinan == 0) {
+            System.out.println("Matriks tidak memiliki invers (determinan nol).");
+            return null; // Keluar jika determinan nol
+        }
+
+        // Hitung kofaktor
+        double[][] kofaktor = {
+                { e * i - f * h, -(b * i - c * h), b * f - c * e },
+                { -(d * i - f * g), a * i - c * g, -(a * f - c * d) },
+                { d * h - e * g, -(a * h - b * g), a * e - b * d }
+        };
+
+        // Transpose kofaktor untuk menghasilkan adjoin
+        double[][] adjoin = new double[3][3];
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                adjoin[j][k] = kofaktor[k][j];
+            }
+        }
+
+        // Hitung invers matriks
+        double[][] invers = new double[3][3];
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                invers[j][k] = adjoin[k][j] / determinan;
+            }
+        }
+
+        return invers;
+    }
+
+    public static void printHasil(double[][] matriksHasil) {
+        for (int i = 0; i < matriksHasil.length; i++) {
+            for (int j = 0; j < matriksHasil[0].length; j++) {
+                System.out.print(matriksHasil[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
